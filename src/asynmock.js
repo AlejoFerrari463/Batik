5c848230239a64fcf6a530b3506a2135f7419742
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-09-11 09:23:50 -0300

    9 productos en total. 3 por categoria. listo para PreEntrega 2

--- src/asynmock.js
+++ src/asynmock.js
@@ -2,9 +2,14 @@ const misProductos = [
 
     { id:1, nombre: "Zapatillas Nike Jordan 1", precio: 200000, imagen:"../img/1-ZapasNikeJordan.png", categoria: "zapatillas",descripcion: "Jordan 1 Para Hombres Retro" },
     { id:2, nombre: "Campera Adidas Beckenbauer", precio: 150000, imagen: "../img/2-CamperaAdidas.webp", categoria: "camperas", descripcion: "Campera Retro Alemania VerdeAmarilla" },
-    { id:3, nombre: "Rompe Viento Nike Repel", precio: 100000, imagen: "../img/3-RompeViento.jfif", categoria: "camperas" },
-    { id:4, nombre: "Lebron XXI", precio: 230000, imagen: "../img/4-ZapatillasLebron.webp", categoria: "zapatillas" },
-    { id:5, nombre: "Remera Retro Barcelona", precio: 70000, imagen: "../img/5-RemeraRetro.jpg" , categoria: "remeras"},
+    { id:3, nombre: "Rompe Viento Nike Repel", precio: 100000, imagen: "../img/3-RompeViento.jfif", categoria: "camperas", descripcion: "Rompeviento Nike Anti Agua" },
+    { id:4, nombre: "Lebron XXI", precio: 230000, imagen: "../img/4-ZapatillasLebron.webp", categoria: "zapatillas", descripcion: "Lebron XXI Moradas" },
+    { id:5, nombre: "Remera Retro Barcelona", precio: 70000, imagen: "../img/5-RemeraRetro.jpg" , categoria: "remeras", descripcion: "Remera Retro Barcelona (Henry)"},
+    { id:6, nombre: "Remera Adidas Flame", precio: 80000, imagen: "../img/6-AdidasFlame.jfif" , categoria: "remeras", descripcion: "Remera Adidas Flame"},
+    { id:7, nombre: "Adidas Campus Green", precio: 180000, imagen: "../img/7-CampusGreen.webp" , categoria: "zapatillas"},
+    { id:8, nombre: "Campera Chicago Bulls", precio: 400000, imagen: "../img/8-CamperaBulls.jpg" , categoria: "camperas", descripcion: "Chicago Bulls Retro"},
+    { id:9, nombre: "Remera Oversize", precio: 90000, imagen: "../img/9-RemeraOver.webp" , categoria: "remeras", descripcion: "Oversize Bear"},
+   
 
 ]
 
