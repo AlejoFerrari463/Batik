bfcc87406eda6f38de46664dd8439de3795178e2
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-09-12 19:30:52 -0300

    counter agregado. falta darle estilo

--- src/componentes/Counter/Counter.jsx
+++ src/componentes/Counter/Counter.jsx
@@ -3,9 +3,52 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 
 
-const Counter = () => {
+const Counter = ({inicial, stock, funcionAgregar}) => {
+
+    const [contador, setContador] = useState(inicial)
+
+    const sumarContador = ()=>{
+
+
+        if (contador<stock){
+
+            setContador(contador+1)
+
+        }
+
+       
+
+    }
+
+    const restarContador = ()=>{
+
+        if(contador>inicial){
+            setContador(contador-1)
+        }
+
+       
+
+    }
+
+
+
   return (
-    <div>Counter</div>
+
+
+    <div>
+
+        <button onClick={sumarContador} >+</button>
+        <div>{contador}</div>
+        <button onClick={restarContador} >-</button>
+
+
+        <button onClick={()=>funcionAgregar(contador)} >Aceptar</button>
+
+
+    </div>
+
+
+    
   )
 }
 
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./ItemDetail.css"
 import "../Item/Item.css"
+import Counter from '../Counter/Counter'
+import { Link } from 'react-router-dom'
 
-const ItemDetail = ({id,nombre,imagen,precio,descripcion}) => {
+const ItemDetail = ({id,nombre,imagen,precio,descripcion,stock}) => {
+
+  const [agregarCantidad , setAgregarCantidad] = useState(0)
+
+
+  const funcionAgregar = (cantidad) =>{
+
+
+    setAgregarCantidad(cantidad)
+    console.log("Productos: "+cantidad)
+
+
+  }
+
+
   return (
     
 
@@ -28,6 +44,16 @@ const ItemDetail = ({id,nombre,imagen,precio,descripcion}) => {
         </div>
 
         <h3 className='text-center' >{descripcion}</h3>
+
+        {
+
+
+          agregarCantidad > 0 ? <button>Ir al carro</button> :  <Counter inicial={1} stock={stock} funcionAgregar={funcionAgregar} />
+
+
+        }
+
+       
 
       </div>
         
