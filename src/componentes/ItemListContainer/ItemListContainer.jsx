9590720b9a0ea870fa19bf2632cac831f18342fa
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-10-01 20:12:38 -0300

    implementacion de toogler

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -48,24 +48,17 @@ const ItemListContainer = () => {
 
 
 
-    // obtenerInfo(idCategoria)
-    // .then((response)=>setProductos(response))
-    // .catch((error)=>console.log(error))
-    // .finally(()=>setLoader(false))
-
-
   },[idCategoria])
   
   return (
 
     <div className='container-fluid' >
 
-      {/* <Loader/> */}
 
-      <h1 className='text-center mt-5 mb-5' >MIS PRODUCTOS</h1>
-      
+      {idCategoria ? <h1 className='text-center mt-5 mb-5' >{idCategoria.toUpperCase()}</h1> : <h1 className='text-center mt-5 mb-5' >TODOS LOS PRODUCTOS</h1>}      
       {loader ? <Loader/> :  <ItemList produ={productos} />}
       
+
       
       
     </div>
--- src/componentes/Navbar/NavBar.jsx
+++ src/componentes/Navbar/NavBar.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./NavBar.css"
 import Brand from '../Brand/Brand'
 import CartWidget from '../CartWidget/CartWidget'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+
+
 
 const NavBar = () => {
+
+  const [fondo, setFondo] = useState("black")
+
+  const manejarToogler = (e)=>{
+
+
+   
+    if (e.target.checked == true){
+      setFondo("white")
+      console.log(fondo)
+    }
+    else {
+      setFondo("black")
+      console.log(fondo)
+    }
+
+  }
+
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-fondo">
+    <nav className="navbar navbar-expand-lg" style={{backgroundColor: fondo}} >
     <div className="container-fluid">
     <Brand/>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,11 +44,15 @@ const NavBar = () => {
             <Link className='nav-links' to="/categoria/remeras" > Remeras </Link>
             </li>
             <li className="nav-item">
-            <Link className='nav-links'  to="/categoria/zapatillas" > Zapatillas </Link>
+            <Link className='nav-links' style={{color: "red"}}  to="/categoria/zapatillas" > Zapatillas </Link>
             </li>
-            
         </ul>
         </div>
+
+        <div className="form-check form-switch">
+          <input onChange={manejarToogler} class="form-check-input" type="checkbox" role="switch" />
+        </div>
+
         <CartWidget/>
     </div>
     </nav>
