e8323c4fb31a7cf33330358e80a6f6b5fe90c90f
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-10-04 08:42:52 -0300

    faltaba 1 variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import NavBar from "./componentes/Navbar/NavBar"
 import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailContainer"
-import { CarritoProvider } from "./context/carritoContext"
+
 import Cart from "./componentes/Cart/Cart"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Checkout from "./componentes/Checkout/Checkout"
 import Error404 from "./componentes/Error404/Error404"
+import { CarritoProvider } from "./context/carritoContext"
 
 
 
